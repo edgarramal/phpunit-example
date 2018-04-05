72667c5ce3d3c1675553ca736cddcce4a77d95b0
Edgar Ramal <edgar.ramal@gmail.com>
2018-04-05 23:46:17 +0200

    add autoload with namespace in composer

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-foreach(scandir('src/') as $file) {
-    if(is_file("src/".$file)){
-        require_once("src/".$file);
-    }
-}
--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -3,12 +3,22 @@
 use PHPUnit\Framework\TestCase;
 
 use src\Calculator;
+
 class CalculatorTest extends TestCase
 {
+    private $calculator;
+    public function __construct()
+    {
+        $this->calculator = new Calculator();
+    }
+
     public function testSum()
     {
-        $calculator = new Calculator();
+        $this->assertSame(20, $this->calculator->Sum(15, 5));
+    }
 
-        $this->assertSame(20, $calculator->Sum(15,5));
+    public function testMod()
+    {
+        $this->assertSame(0, $this->calculator->Mod($a,$b));
     }
 }
